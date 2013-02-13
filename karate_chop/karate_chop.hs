01acac9c20fd5ba5a4eb93f55e0ae978a3aa2995
Tomohiko Himura <eiel.hal@gmail.com>
2013-02-13 15:35:55 +0900

    Code Kata Two - 空手チョップ！ 1日目

---
+++ karate_chop/karate_chop.hs
@@ -0,0 +1,61 @@
+import Data.Array
+
+main = undefined
+
+-- |
+-- 2分探索を5種類の方法で実装する その1
+-- 添字を利用して配列を中を回す感じに。
+-- >>> chop 3 []
+-- -1
+-- >>> chop 3 [1]
+-- -1
+-- >>> chop 1 [1]
+-- 0
+-- >>> chop 1 [1, 3, 5]
+-- 0
+-- >>> chop 3 [1, 3, 5]
+-- 1
+-- >>> chop 5 [1, 3, 5]
+-- 2
+-- >>> chop 0 [1,3, 5]
+-- -1
+-- >>> chop 2 [1, 3, 5]
+-- -1
+-- >>> chop 4 [1, 3, 5]
+-- -1
+-- >>> chop 6 [1, 3, 5]
+-- -1
+-- >>> chop 1 [1, 3, 5, 7]
+-- 0
+-- >>> chop 3 [1, 3, 5, 7]
+-- 1
+-- >>> chop 5 [1, 3, 5, 7]
+-- 2
+-- >>> chop 7 [1, 3, 5, 7]
+-- 3
+-- >>> chop 0 [1, 3, 5, 7]
+-- -1
+-- >>> chop 2 [1, 3, 5, 7]
+-- -1
+-- >>> chop 4 [1, 3, 5, 7]
+-- -1
+-- >>> chop 6 [1, 3, 5, 7]
+-- -1
+-- >>> chop 8 [1, 3, 5, 7]
+-- -1
+chop :: Int -> [Int] -> Int
+chop x xs = if end == -1 then -1 else bsearch array (0,end) x
+  where
+    array = listArray (0, end) xs
+    end = length xs - 1
+
+bsearch :: Array Int Int -> (Int, Int) -> Int -> Int
+bsearch array (start,end) x
+  | start == end = if array ! start == x then start else -1
+  | start > end = -1
+  | x == n = half
+  | x < n = bsearch array (start, half-1) x
+  | x > n = bsearch array (half+1, end) x
+    where
+      half = (end+start) `div` 2
+      n = array ! half
